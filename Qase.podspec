4240676e83aa4dbdebceb2cfd625e1f9a4f38289
Mikhail Golovko <mihailmvg@gmail.com>
2020-08-17 12:41:10 +0300

    Fix add config

--- Qase.podspec
+++ Qase.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'Qase'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'Qase'
   s.swift_version    = '5.0'
   
--- Qase/Classes/XCTestCase+Extension.swift
+++ Qase/Classes/XCTestCase+Extension.swift
@@ -3,18 +3,16 @@ import XCTest
 public extension XCTestCase {
     
     func startQaseTest(id: Int) {
-    
-        addTeardownBlock { [weak self] in
-            var config = [String: Any]()
-            config["caseId"] = id
-            
-            guard let data = try? JSONSerialization.data(withJSONObject: config, options: []) else { return }
-            let attachment = XCTAttachment(data: data)
-            attachment.name = "Qase config"
-            attachment.lifetime = .keepAlways
-            
-            self?.add(attachment)
-        }
+
+        var config = [String: Any]()
+        config["caseId"] = id
+        
+        guard let data = try? JSONSerialization.data(withJSONObject: config, options: []) else { return }
+        let attachment = XCTAttachment(data: data)
+        attachment.name = "Qase config"
+        attachment.lifetime = .keepAlways
+        
+        add(attachment)    
     }
     
     func step(_ name: String? = nil, block: (XCTActivity) -> Void) {
